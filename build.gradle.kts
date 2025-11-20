978428c15f4493530a19d6fda2a914d3a0872b7f
HAJJI AMIN <amin.hajji@microej.com>
2025-11-20 14:35:33 +0100

    Removed SNAPSHOT from plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
-    id("com.microej.gradle.module-repository") version "1.5.0-SNAPSHOT"
+    id("com.microej.gradle.module-repository") version "1.5.0"
 }
 
 group = "com.mycompany"
 version = "0.1.0-RC"
 
-microej {
-    dependenciesSkippedCheckers.set(mapOf("ej.api:edc:1.3.7" to "changelog,retrieve"))
-}
-
 dependencies {
-    microejModule("ej.api:edc:1.3.7")
+    //Uncomment to skip the checks on this dependency
+    microejModule("ej.api:edc:1.3.7")//?.let {
+    //  microej.skipCheckers(it, "readme,license,changelog")
+    //}
 
     //Uncomment to include multiple versions of EDC in your project
     //microejModule("ej.api:edc:1.3.3")
@@ -19,7 +18,4 @@ dependencies {
     //microejModule("ej.api:bon:1.4.4") {
     //  isTransitive = false
     //}
-
-    //Uncomment to include a single artifact of a dependency
-    //microejModule("org.example:my-kernel:1.0.0@zip")
 }
